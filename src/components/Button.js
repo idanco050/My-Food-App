a55780474fcafb99db0b62249eacc35005258cdb
idanco <idanco050@gmail.com>
2023-01-12 21:55:21 +0200

    Cart button update

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,22 @@
 
-import {Fragment} from 'react'
+import {Fragment,useContext} from 'react'
 import classes from './HeaderCartButton.module.css'
+import CartContext from './CartContext'
+
 
 const Button = props => 
 {
+    const{cart} = useContext(CartContext)
+
+    let totalItems = 0;
+    cart.map((item) => {
+        totalItems += item.amount
+    });
     return(
 <Fragment>
     <button className = {classes.button} onClick ={props.showCart}>
-        {props.label}
+        {"Cart"}
+        <span  className = {classes.badge}>{totalItems}</span>
     </button>
 </Fragment>
     );
